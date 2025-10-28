b7f541d51c02716573e8cbdff5a88a9b10a398bd
Victor-sarti <Sartivictor24@gmail.com>
2025-10-28 11:54:18 -0300

    config type and tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="h-screen flex flex-col items-center justify-center bg-gradient-to-br from-indigo-500 to-purple-600 text-white">
+      <h1 className="text-5xl font-bold">TechPet 🐾</h1>
+      <p className="mt-4 text-lg">Tailwind 4 funcionando perfeitamente!</p>
+    </div>
+  );
+}
+
+export default App;
